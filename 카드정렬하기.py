d04728fc860a0959bbbc51b66aa52b4d4d27c332
andychyou <andychyou@naver.com>
2023-10-06 18:36:42 +0900

    dd

--- 카드정렬하기.py
+++ 카드정렬하기.py
@@ -1,14 +1,31 @@
 import sys
+from collections import deque
+from heapq import *
 
 sys.stdin = open('test.txt','r')
 input = sys.stdin.readline
 
+N = 0
+
+
 
 def main():
+    global N
     N = int(input())
     cards = []
     for x in range(N):
-        cards.append(int(input()))
+        heappush(cards, int(input()))
+    answer = 0
+    while 1 < len(cards):
+        a = heappop(cards)
+        b = heappop(cards)
+        new = a + b
+        answer += new
+        heappush(cards, new)
+    print(answer)
+
+    
+    
     
 
 if __name__ == '__main__':
