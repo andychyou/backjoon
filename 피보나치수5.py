9faeeea1283e653abeeba752c5f4928f0b0b4da0
andychyou <andychyou@naver.com>
2023-10-07 23:26:38 +0900

    피보나치 메모이제이션 적용

--- 피보나치수5.py
+++ 피보나치수5.py
@@ -3,19 +3,21 @@ from collections import deque
 from itertools import combinations
 
 input = sys.stdin.readline
-
+cache = []
 
 def Fib(n):
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
-    else:
-        return Fib(n-1) + Fib(n-2)
+    global cache
+    if cache[n] == -1:
+        cache[n] = Fib(n-1) + Fib(n-2)
 
+    return cache[n]
 
 def main():
+    global cache
     N = int(input())
+    cache = [-1] * (N+1)
+    cache[0] = 0
+    cache[1] = 1
     print(Fib(N))
 
 
