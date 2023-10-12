a54f2ae51a5fe06944285154ace67e0e420fe52b
andychyou <andychyou@naver.com>
2023-10-12 23:38:55 +0900

    dd

--- N-Queen.py
+++ N-Queen.py
@@ -6,36 +6,28 @@ import copy
 
 answer = 0
 
-def Check(N, k, col):
-    flag = True
-    for i in range(N):
-        if col[i] != 0:
-            if col[i] == col[k] or y == x - i + j or y == -(x-i) + j:
-                flag = False
-                break
-    return flag
-
-def Backtrack( col, N, k):
-    global answer
-    if k == N:
-        answer += 1
-        return
-    if Check(N,k,col):
-        for c in range(N):
-            temp_col = col[:]
-            temp_col[k] = c
-            Backtrack(temp_col, N, k+1)
-
-
-
-
 
 
 def main():
     global answer
     N = int(input())
     # k 1 - 8
-    col = [0]*N
+    col = [-1]*N
+
+    def check(i,j,col):
+        for c in range(len(col)):
+            if col[c]
+
+    
+    def Backtrack(k, col):
+        nonlocal N
+        if k == N:
+            return
+        for i in range(N):
+            for j in range(N):
+                col_temp = col[:]
+                col_temp[i] = j
+                Backtrack(k+1, col_temp)
     Backtrack(col,N,0)
     print(answer)
 
